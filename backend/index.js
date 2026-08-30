da5ac2c1f5328839cc050a606c8312b2361565c8
Proshanta15 <roy15-6997@diu.edu.bd>
2026-08-30 09:09:40 +0600

    pern-todo

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import cors from "cors";
+import express from "express";
+import todoRoutes from "./routes/todos.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/todos", todoRoutes);
+
+app.listen(5000, () => {
+  console.log("Server is listening on port 5000");
+});
---
+++ backend/routes/todos.js
@@ -0,0 +1,61 @@
+import { Router } from "express";
+import pool from "../db.js";
+
+const router = Router();
+
+// Create todo
+router.post("/", async (req, res) => {
+  try {
+    const { description, completed } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description, completed) VALUES ($1, $2) RETURNING *",
+      [description, completed || false],
+    );
+    res.json(newTodo.rows[0]);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// Get All todos
+
+router.get("/", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
+    res.json(allTodos.rows);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// Update todo
+router.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description, completed } = req.body;
+    const updatedTodo = await pool.query(
+      "UPDATE todo SET description = $1, completed = $2 WHERE todo_id = $3 RETURNING *",
+      [description, completed, id],
+    );
+    res.json(updatedTodo.rows[0]);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// Delete a todo
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    await pool.query("DELETE FROM todo WHERE todo_id = $1", [id]);
+    res.json("Todo was deleted!");
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+export default router;
